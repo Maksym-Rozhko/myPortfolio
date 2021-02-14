17ca6e392d233a9a5453e70785c047623585de2e
Maksym <maksymRozhko7@gmail.com>
2021-02-15 01:37:16 +0200

    fix(responsive);

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 new WOW().init();
 
 var mySwiper = new Swiper('.swiper-container', {
+    spaceBetween: 10,
     pagination: {
         el: '.projects-pagination',
         bulletClass: 'projects-bullet',

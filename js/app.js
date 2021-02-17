32fac3a4b27b3d4614886a300f165ec58cb1eabf
Maksym <maksymRozhko7@gmail.com>
2021-02-18 00:05:32 +0200

    feat(form-validation);
    - simple validation on the name and phone.

--- js/app.js
+++ js/app.js
@@ -9,3 +9,42 @@ let mySwiper = new Swiper('.swiper-container', {
         clickable: true
       },
 });
+
+const inputNameEl = document.querySelector('.input-name');
+const inputPhoneEl = document.querySelector('.input-phone');
+const textareaEl = document.querySelector('.text-message');
+const formBtnEl = document.querySelector('.form-button');
+
+function InputInvalidStyles(input) {
+  input.style.fontSize = '15px';
+  input.style.outlineColor = 'red';
+  input.value = '';
+  input.focus();
+}
+
+function userName(name) {
+  let letters = /^[A-Za-z]+$/;
+  if(name.value.match(letters)){
+    return true;
+  } else {
+    name.placeholder = 'must have letters alphabet only';
+    InputInvalidStyles(name)
+    return false;
+  }
+}
+
+function userPhone(phone) {
+  let digits = /^\d+$/;
+  if(phone.value.match(digits)){
+    return true;
+  } else {
+    phone.placeholder = 'Only digits of your phone number';
+    InputInvalidStyles(phone)
+    return false;
+  }
+}
+
+formBtnEl.addEventListener('click', (e) => {
+  userName(inputNameEl);
+  userPhone(inputPhoneEl);
+});
